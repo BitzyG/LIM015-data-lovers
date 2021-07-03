11de2f1fd474cf04a8d6b46a797b744069dc9de0
AM27TH <amelanie.trillo27@gmail.com>
2021-07-03 15:21:37 -0500

    add events for filtering by director/producer and film search engine

--- src/main.js
+++ src/main.js
@@ -1,29 +1,81 @@
-// import { example } from './data.js';
+import { searchDataFilms, sortData , filterDataByDirectorProducer } from './data.js';
 // import data functions
 import data from './data/ghibli/ghibli.js';
-// import data
 
 // Selectors DOM
+const searchFilms = document.getElementById('searchFilm');
+const filterByDirectorProducer = document.getElementById("filterByDirectorProducer");
+const sortBy = document.querySelector("#sortBy");
+const orderData = document.getElementById("orderData");
+const counter = document.querySelector(".menuBar__showing");
 const filmsContainer = document.querySelector('.container__films');
-const counter = document.querySelector(".container__showing");
+
+// Default variables
+let dataFilms = data.films;
+let sortOrder = 'asc';
 
 function showDataFilms(films){
-    //agregar funcion para limpiar para limpiar
     filmsContainer.innerHTML = "";
-    // Inicio de todo
-    films.forEach(film => {
-        const container = document.createElement('section');
-        container.className = 'container__card';
-        container.innerHTML = `
-            <section class='card__title'>
-                <h1>${film.title}</h1>
-            </section>
-            <img class='card__image' src='${film.poster}' alt='${film.title}' title='${film.title}' loading='lazy'>
-        `;
-        filmsContainer.appendChild(container);
-    })
+    if(films.length === 0){
+        filmsContainer.innerHTML = "Film not found";
+    }else{
+        // Ordered data
+        const sortedData = sortData(films, sortBy.value, sortOrder)
+        // Show Data
+        sortedData.forEach(film => {
+            const container = document.createElement('section');
+            container.className = 'container__card';
+            container.innerHTML = `
+                <section class='card__title'>
+                    <h1>${film.title}</h1>
+                </section>
+                <img class='card__image' src='${film.poster}' alt='${film.title}' title='${film.title}' loading='lazy'>
+            `;
+            filmsContainer.appendChild(container);
+            // Add modal
+        });
+    }
     counter.innerHTML = `Showing ${films.length}`;
-    /* console.log(film.title);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    showDataFilms(sortData(dataFilms, sortBy.value, sortOrder));
+    addFilterByDirectorProducer();
+});
+
+// Order Films
+sortBy.addEventListener("change", () => {
+    showDataFilms(dataFilms);
+});
+
+// Order Data
+orderData.addEventListener("click", () => {
+    sortOrder = (orderData.classList.length === 3 ? 'asc' : 'desc');
+    orderData.classList.toggle("fa-sort-amount-up-alt");
+    showDataFilms(dataFilms);
+});
+
+// Filter Director and Producer
+filterByDirectorProducer.addEventListener("change", ()=>{
+    searchFilms.value = "";
+    if(filterByDirectorProducer.value === 'all'){
+        dataFilms = data.films;
+    }else{
+        dataFilms = filterDataByDirectorProducer(data.films,filterByDirectorProducer.value);
+    }
+    showDataFilms(dataFilms);
+});
+
+// Search By Film
+searchFilms.addEventListener('keyup', ()=>{
+    dataFilms = searchDataFilms(data.films, searchFilms.value.toLowerCase());
+    showDataFilms(dataFilms);
+});
+
+
+/*
+//Usaremos esta parte para la segunda historia de usuario
+    console.log(film.title);
     console.log(film.poster);
     film.people.map(person => {
         console.log(person.name);
@@ -35,28 +87,29 @@ function showDataFilms(films){
         })
     }else{
         console.log("No tiene locación");
-    } */
+    }
+*/
+
+
+function addFilterByDirectorProducer(){
+    const namesDirectorProducer = getNamesDirectorProducer(data.films);
+    for (const name of namesDirectorProducer){
+        var option = document.createElement("option");
+        option.value = name;
+        option.text = name;
+        filterByDirectorProducer.appendChild(option);
+    }
 }
 
-showDataFilms(data.films);
-
-//funcion filtrar
-function deleteDuplicate(data){
-    return data.filter((item, index)=>{
-        return data.indexOf(item) === index;
+function getNamesDirectorProducer(films){
+    const namesDirectorProducer = [];
+    /* Add names of Directors and Producers */
+    films.forEach(film => {
+        namesDirectorProducer.push(film.director);
+        namesDirectorProducer.push(film.producer);
+    });
+    /* Delete Duplicates */
+    return namesDirectorProducer.filter((item, index)=>{
+        return namesDirectorProducer.indexOf(item) === index;
     });
 }
-let directorProducer = (arr) => {
-    let newArray = [];
-    arr.forEach(arr => {
-        newArray.push(arr.director);
-        newArray.push(arr.producer);
-    })
-    return deleteDuplicate(newArray);
-}
-
-console.log(directorProducer(data.films));
-const array = directorProducer(data.films);
-
-let result = data.films.filter(film => (film.director === array[8] || film.producer === array[8] ));
-console.log(result);

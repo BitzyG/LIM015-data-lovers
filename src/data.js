fb8bf7d6d536ebdc832aa2d07b52316023038b93
AM27TH <amelanie.trillo27@gmail.com>
2021-07-03 15:20:53 -0500

    adds filters by director/producer and film search engine

--- src/data.js
+++ src/data.js
@@ -3,3 +3,11 @@ export const sortData = (data, sortBy, sortOrder) => {
   const order = (sortOrder === 'asc') ? 1 : -1;
   return data.sort((a, b) => (a[sortBy] > b[sortBy] ? order: (-order)));
 };
+
+export const filterDataByDirectorProducer = (data, condition) => {
+  return data.filter(film => (film.director === condition || film.producer === condition));
+};
+
+export const searchDataFilms = (data, condition) => {
+  return data.filter(film => (film.title.toLowerCase().indexOf(condition)!==-1));
+}

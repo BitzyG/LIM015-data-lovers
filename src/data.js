536e76815e8b298ec5e419ee5b753eac295ef209
AM27TH <amelanie.trillo27@gmail.com>
2021-07-08 20:00:49 -0500

    Update function searchData

--- src/data.js
+++ src/data.js
@@ -1,13 +1,20 @@
 //function Order by to order data
 export const sortData = (data, sortBy, sortOrder) => {
   const order = (sortOrder === 'asc') ? 1 : -1;
-  return data.sort((a, b) => (a[sortBy] > b[sortBy] ? order: (-order)));
+  if(sortBy == 'name' || sortBy == 'title'){
+    return data.sort((a, b) => (a[sortBy] > b[sortBy] ? order: (-order)));
+  }
+  return data.sort((a, b) => (parseInt(a[sortBy]) > parseInt(b[sortBy]) ? order: (-order)));
 };
 
 export const filterDataByDirectorProducer = (data, condition) => {
   return data.filter(film => (film.director === condition || film.producer === condition));
 };
 
-export const searchDataFilms = (data, condition) => {
-  return data.filter(film => (film.title.toLowerCase().indexOf(condition)!==-1));
+export const filterDataBy = (data, condition, value) => {
+  return data.filter(film => (film[condition] === value));
+};
+
+export const searchData = (data, condition, value) => {
+  return data.filter(item => (item[condition].toLowerCase().includes(value)));
 }
